9b65c7e9f75cd164c747c9d633a2e71a74c47b5e
Reo Sahobby <reoshby@gmail.com>
2022-04-05 12:50:51 +0700

    Update GeneralRepository

---
+++ LMS/Repository/GeneralRepository.cs
@@ -0,0 +1,48 @@
+﻿using LMS.Context;
+using LMS.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LMS.Repository
+{
+    public class GeneralRepository<Context, Entity, Key> : IRepository<Entity, Key>
+        where Entity : class
+        where Context : MyContext
+    {
+        private readonly MyContext myContext;
+        private readonly DbSet<Entity> entities;
+
+        //Constructor
+        public GeneralRepository(MyContext myContext)
+        {
+            this.myContext = myContext;
+            entities = myContext.Set<Entity>();
+        }
+
+        public int Delete(Key key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Entity> GET()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Entity Get(Key key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Insert(Entity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Update(Entity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ LMS/Repository/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LMS.Repository.Interface
+{
+    public interface IRepository<Entity, Key> where Entity : class
+    {
+        IEnumerable<Entity> GET();
+
+        Entity Get(Key key);
+
+        int Insert(Entity entity);
+
+        int Update(Entity entity);
+
+        int Delete(Key key);
+    }
+}
