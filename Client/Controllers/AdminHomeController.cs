440ef6bdffee3919fdedbeda0117c8aae081fdb0
vdhdk <72961505+vincenhk@users.noreply.github.com>
2022-04-07 12:28:29 +0700

    update

--- Client/Controllers/AdminHomeController.cs
+++ Client/Controllers/AdminHomeController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Client.Controllers
 {
-    public class HomeController : Controller
+    public class AdminHomeController : Controller
     {
         public IActionResult Index()
         {
