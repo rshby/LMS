3193da7b873b0da95d1488b2eeb29ae4552cc1d1
Reo Sahobby <reoshby@gmail.com>
2022-04-11 09:18:21 +0700

    update

--- Client/Base/BaseController.cs
+++ Client/Base/BaseController.cs
@@ -1,20 +1,53 @@
 ﻿using Client.Repositories.Interface;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Client.Base
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController<TEntity, TRepository, TId> : ControllerBase
+    public class BaseController<TEntity, TRepository, TId> : Controller
         where TEntity : class
         where TRepository : IRepository<TEntity, TId>
     {
-        
+        private readonly TRepository repository;
+
+        public BaseController(TRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetAll()
+        {
+            var result = await repository.Get();
+            return Json(result);
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> Get(TId id)
+        {
+            var result = await repository.Get(id);
+            return Json(result);
+        }
+
+        [HttpPost]
+        public JsonResult Post(TEntity entity)
+        {
+            var result = repository.Post(entity);
+            return Json(result);
+        }
+
+        [HttpPut]
+        public JsonResult Put(TEntity entity)
+        {
+            var result = repository.Put(entity);
+            return Json(result);
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(TId id)
+        {
+            var result = repository.Delete(id);
+            return Json(result);
+        }
     }
 }
