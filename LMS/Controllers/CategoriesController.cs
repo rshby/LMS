06166fef25259a9fc6d61e6e145577f2dbbe5e07
Dennyfpr <dennyfpr@gmail.com>
2022-04-12 09:27:43 +0700

    -c

--- LMS/Controllers/CategoriesController.cs
+++ LMS/Controllers/CategoriesController.cs
@@ -1,10 +1,7 @@
 ﻿using LMS.Base;
 using LMS.Models;
 using LMS.Repository.Data;
-<<<<<<< HEAD
-=======
 using LMS.ViewModels;
->>>>>>> Denny
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -12,21 +9,8 @@ using System;
 
 namespace LMS.Controllers
 {
-<<<<<<< HEAD
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoriesController : BaseController<Category, CategoryRepository, int>
-    {
-        private readonly CategoryRepository repository;
-        public IConfiguration configuration;
-
-        //Constructor
-        public CategoriesController(CategoryRepository repository, IConfiguration configuration) : base(repository)
-        {
-            this.repository = repository;
-            this.configuration = configuration;
-        }
-=======
     public class CategoriesController : BaseController<Category, CategoryRepository, int>
     {
         private readonly CategoryRepository categoryRepo;
@@ -36,7 +20,5 @@ namespace LMS.Controllers
             this.categoryRepo = categoryRepo;
             this.configuraion = configuration;
         }
-
->>>>>>> Denny
     }
 }
