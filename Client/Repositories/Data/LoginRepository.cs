8196cdb4d0a0833c1311f3a53f4729e94ce34669
Dennyfpr <dennyfpr@gmail.com>
2022-04-12 13:39:47 +0700

    Add Login Repository

---
+++ Client/Repositories/Data/LoginRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Repositories.Data
+{
+    public class LoginRepository
+    {
+        // Code here
+    }
+}
