8d4f8cffd57d25513505a7ff8366201e0ab7182a
Reo Sahobby <reoshby@gmail.com>
2022-04-13 09:58:27 +0700

    update section dan cek gitignore

--- LMS/Repository/Data/SectionRepository.cs
+++ LMS/Repository/Data/SectionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using LMS.Context;
 using LMS.Models;
+using LMS.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,6 +29,16 @@ namespace LMS.Repository.Data
                 return false; 
             }
         }
+
+        // Master Section
+        public List<MasterSectionVM> AllMasterSectionVM()
+        {
+            var data = myContext.Sections
+                .Join(myContext.Classes, s => s.Class_Id, c => c.Id, (s, c) => new { s, c})
+                .Join(myContext.Levels, sc => sc.c.Level_Id, l => l.Id, (sc, l) => new { sc, l})
+                .Join(myContext.Categories, scl => scl.sc.c.Category_Id, ct => ct.Id, ())
+        }
+
 
         //Menampilkan Semua Section by Class_Id yang diinputkan
         public List<Section> SectionByClassId(int inputClassId)
---
+++ LMS/ViewModels/MasterSectionVM.cs
@@ -0,0 +1,19 @@
+﻿namespace LMS.ViewModels
+{
+    public class MasterSectionVM
+    {
+        public int Section_Id { get; set; }
+        public string Section_Name { get; set; }
+        public int Section_Chapter { get; set; }
+        public string Section_Content { get; set; }
+        public int Class_Id { get; set; }
+        public string Class_Name { get; set; }
+        public string Class_UrlPic { get; set; }
+        public string Class_Desc { get; set; }
+        public int CLass_TotalChapter { get; set; }
+        public int Class_Price { get; set; }
+        public double Class_Rating { get; set; }
+        public string Level_Name { get; set; }
+        public string Category_Name { get; set; }
+    }
+}
